6e082e22fb7169a92c8f4c5845cdfcad4c872013
thanandorn45 <arttyyy41@gmail.com>
2023-06-27 17:25:47 +0700

    Week02

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,11 @@
+<h2>HTML Image</h2>
+<img src=" {{ $ant}} " alt="Trulli" width="300" height="310">
+
+<img src="{{$bird}}" alt="Trulli" width="300" height="310">
+
+<img src="{{$cat}}" alt="Trulli" width="300" height="310">
+
+<img src="{{$god}}" alt="Trulli" width="300" height="310">
+
+<img src="{{$spider}}" alt="Trulli" width="300" height="310">
+</body>
